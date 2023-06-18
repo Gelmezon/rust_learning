8dd3dea0558454dd4ce59a38210a35ea9f2fa8c3
Feitan <caf43rer@gmail.com>
2023-06-19 00:12:10 +0800

    修复web服务的bug

--- web_test/src/main.rs
+++ web_test/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get,post,web,App,HttpServer,HttpResponse};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> HttpResponse {
@@ -15,15 +15,15 @@ async fn manual_hello() -> impl Responder {
 }
 
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-        .service(hello)
-        .service(echo)
-        .route("/hey", web::get().to(manual_hello))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
