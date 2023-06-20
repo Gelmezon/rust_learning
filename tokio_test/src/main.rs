b6d7f13017c0bf8a30c5f77fa4729bea23bb9daf
Feitan <caf43rer@gmail.com>
2023-06-20 22:26:17 +0800

    基于tokio的http服务器

--- tokio_test/src/main.rs
+++ tokio_test/src/main.rs
@@ -1,26 +1,32 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::server::conn::http1;
+use bytes::Bytes;
+use http_body_util::Full;
+use hyper::{Request, Response};
+use hyper::service::service_fn;
 use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 #[tokio::main]
-async fn main(){
-    println!("web Start");
-    let address = "127.0.0.1:8888";
-    let listener = TcpListener::bind(address).await.expect("绑定接口失败");
-    
-    println!("Listening in {}",address);
-
+pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
+    let listener = TcpListener::bind(addr).await?;
     loop {
-        let (mut socket,_) =  listener.accept().await.expect("接受连接失败");
-
-        tokio::spawn(async move{
-            let mut buffer = [0; 1024];
-            let n = socket.read(&mut buffer).await.expect("读取数据失败");
-
-            let req = String::from_utf8_lossy(&buffer[..n]);
-            println!("req: {}",req);
-            let resp = "Hello Tokio";
-            socket.write_all(resp.as_bytes()).await.expect("写入数据失败");
-
-            println!("send resp: {}",resp)
+        let (stream, _) = listener.accept().await?;
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(stream, service_fn(hello))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
         });
     }
+}
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    let resp = Response::builder()
+        .header("Content-type", "text/plain")
+        .body(Full::new(Bytes::from("HELLO WORLD")))
+        .unwrap();
+    Ok(resp)
 }
